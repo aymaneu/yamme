98b7f7a0582af515a5284e513874214380356b4b
aymaneu <elgadaymane@gmail.com>
2024-05-24 02:52:29 +0100

    added header

---
+++ src/pages/components/icons/instagram.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Instagram = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      focusable="false"
+      color='var(--token-db4c9d73-2bf4-4bdb-8d75-f44efab236cc, rgba(33, 33, 33, 0.7)) /* {"name":"Color 2 (70%)"} */'
+    >
+      <g color='var(--token-db4c9d73-2bf4-4bdb-8d75-f44efab236cc, rgba(33, 33, 33, 0.7)) /* {"name":"Color 2 (70%)"} */'>
+        <path d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z"></path>
+      </g>
+    </svg>
+  );
+};
+
+export default Instagram;
