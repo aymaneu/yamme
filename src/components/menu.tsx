afb17c30988f4c801fd75a02211cc0114f139f5d
aymaneu <elgadaymane@gmail.com>
2024-05-27 04:32:59 +0100

    working on animation

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -2,10 +2,53 @@ import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { playfair } from "./header";
 import { content } from "@/data/menu";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 const Menu = () => {
+  const appetizersRef = useRef(null);
+  const appetizersRefView = useInView(appetizersRef, {
+    margin: "-50% 0% 50% 0%",
+  });
+
+  const saladsRef = useRef(null);
+  const saladsRefView = useInView(saladsRef, {
+    margin: "-50% 0% 50% 0%",
+  });
+  const entreesRef = useRef(null);
+  const entreesRefView = useInView(entreesRef, {
+    margin: "-50% 0% 50% 0%",
+  });
+  const sidesRef = useRef(null);
+  const sidesRefView = useInView(sidesRef, {
+    margin: "-50% 0% 50% 0%",
+  });
+  const dessertsRef = useRef(null);
+  const dessertsView = useInView(dessertsRef, {
+    margin: "-50% 0% 50% 0%",
+  });
+  const [currentSec, setCurrentSec] = useState("appetizers");
+  useEffect(() => {
+    if (appetizersRefView) {
+      setCurrentSec("Appetizers");
+    } else if (saladsRefView) {
+      setCurrentSec("Salads");
+    } else if (entreesRefView) {
+      setCurrentSec("Entrees");
+    } else if (sidesRefView) {
+      setCurrentSec("Sides");
+    } else if (dessertsView) {
+      setCurrentSec("Desserts");
+    }
+  }, [
+    appetizersRefView,
+    dessertsView,
+    entreesRefView,
+    saladsRefView,
+    sidesRefView,
+  ]);
+  console.log(currentSec);
   return (
     <div>
       <p
@@ -21,7 +64,12 @@ const Menu = () => {
           {content.map((i) => {
             return (
               <Link
-                className="border-t border-l border-r p-2 border-dashed rounded-t-md border-subOrange"
+                className={cn(
+                  " p-2 rounded-t-md border-t border-l border-r",
+                  currentSec === i.label
+                    ? "border-dashed border-subOrange"
+                    : "border-background"
+                )}
                 href={i.link}
                 key={i.label}
               >
@@ -31,58 +79,253 @@ const Menu = () => {
           })}
         </div>
         <div>
-          {content.map((j) => (
-            <div className="mt-36 z-10" key={j.label}>
-              <p
-                className={cn(
-                  "text-center mb-10 text-secondary/50 text-2xl italic",
-                  playfair.className
-                )}
-              >
-                {j.label}
-              </p>
-              <div className="flex px-28 flex-col gap-7">
-                {j.info.map((k) => {
-                  return (
+          <div className="mt-36 z-10">
+            {/* //? one */}
+            <p
+              className={cn(
+                "text-center mb-10 text-secondary/50 text-2xl italic",
+                playfair.className
+              )}
+            >
+              {content[0].label}
+            </p>
+            <div ref={appetizersRef} className="flex px-28 flex-col gap-7">
+              {content[0].info.map((k) => {
+                return (
+                  <motion.div
+                    whileHover="hover"
+                    className="flex cursor-pointer gap-8 justify-between"
+                    key={k.label}
+                  >
                     <motion.div
-                      whileHover="hover"
-                      className="flex cursor-pointer gap-8 justify-between"
-                      key={k.label}
+                      className="w-52 h-36 z-10 shrink-0"
+                      transition={{ ease: "easeInOut", duration: 0.4 }}
+                      variants={{ hover: { scale: 1.2 } }}
                     >
-                      <motion.div
-                        className="w-52 h-36 z-10 shrink-0"
-                        transition={{ ease: "easeInOut", duration: 0.4 }}
-                        variants={{ hover: { scale: 1.2 } }}
-                      >
-                        <Image
-                          src={k.image}
-                          alt="image"
-                          width={300}
-                          height={300}
-                          className="w-52 shrink-0 h-36 object-cover rounded-md"
-                        />
-                      </motion.div>
-                      <div className="grow">
-                        <div
-                          className={cn(
-                            "flex text-subOrange text-2xl items-center",
-                            playfair.className
-                          )}
-                        >
-                          <p className="shrink-0">{k.label}</p>
-                          <span className="border-t border-subOrange border-dashed w-full" />
-                          <p>${k.price}</p>
-                        </div>
-                        <p className="text-secondary/50 italic text-base">
-                          {k.description}
-                        </p>
-                      </div>
+                      <Image
+                        src={k.image}
+                        alt="image"
+                        width={300}
+                        height={300}
+                        className="w-52 shrink-0 h-36 object-cover rounded-md"
+                      />
                     </motion.div>
-                  );
-                })}
-              </div>
+                    <div className="grow">
+                      <div
+                        className={cn(
+                          "flex text-subOrange text-2xl items-center",
+                          playfair.className
+                        )}
+                      >
+                        <p className="shrink-0">{k.label}</p>
+                        <span className="border-t border-subOrange border-dashed w-full" />
+                        <p>${k.price}</p>
+                      </div>
+                      <p className="text-secondary/50 italic text-base">
+                        {k.description}
+                      </p>
+                    </div>
+                  </motion.div>
+                );
+              })}
             </div>
-          ))}
+            {/* //? two */}
+            <p
+              className={cn(
+                "text-center mb-10 text-secondary/50 text-2xl italic",
+                playfair.className
+              )}
+            >
+              {content[1].label}
+            </p>
+            <div ref={saladsRef} className="flex px-28 flex-col gap-7">
+              {content[1].info.map((k) => {
+                return (
+                  <motion.div
+                    whileHover="hover"
+                    className="flex cursor-pointer gap-8 justify-between"
+                    key={k.label}
+                  >
+                    <motion.div
+                      className="w-52 h-36 z-10 shrink-0"
+                      transition={{ ease: "easeInOut", duration: 0.4 }}
+                      variants={{ hover: { scale: 1.2 } }}
+                    >
+                      <Image
+                        src={k.image}
+                        alt="image"
+                        width={300}
+                        height={300}
+                        className="w-52 shrink-0 h-36 object-cover rounded-md"
+                      />
+                    </motion.div>
+                    <div className="grow">
+                      <div
+                        className={cn(
+                          "flex text-subOrange text-2xl items-center",
+                          playfair.className
+                        )}
+                      >
+                        <p className="shrink-0">{k.label}</p>
+                        <span className="border-t border-subOrange border-dashed w-full" />
+                        <p>${k.price}</p>
+                      </div>
+                      <p className="text-secondary/50 italic text-base">
+                        {k.description}
+                      </p>
+                    </div>
+                  </motion.div>
+                );
+              })}
+            </div>{" "}
+            {/* //? three */}
+            <p
+              className={cn(
+                "text-center mb-10 text-secondary/50 text-2xl italic",
+                playfair.className
+              )}
+            >
+              {content[2].label}
+            </p>
+            <div ref={entreesRef} className="flex px-28 flex-col gap-7">
+              {content[2].info.map((k) => {
+                return (
+                  <motion.div
+                    whileHover="hover"
+                    className="flex cursor-pointer gap-8 justify-between"
+                    key={k.label}
+                  >
+                    <motion.div
+                      className="w-52 h-36 z-10 shrink-0"
+                      transition={{ ease: "easeInOut", duration: 0.4 }}
+                      variants={{ hover: { scale: 1.2 } }}
+                    >
+                      <Image
+                        src={k.image}
+                        alt="image"
+                        width={300}
+                        height={300}
+                        className="w-52 shrink-0 h-36 object-cover rounded-md"
+                      />
+                    </motion.div>
+                    <div className="grow">
+                      <div
+                        className={cn(
+                          "flex text-subOrange text-2xl items-center",
+                          playfair.className
+                        )}
+                      >
+                        <p className="shrink-0">{k.label}</p>
+                        <span className="border-t border-subOrange border-dashed w-full" />
+                        <p>${k.price}</p>
+                      </div>
+                      <p className="text-secondary/50 italic text-base">
+                        {k.description}
+                      </p>
+                    </div>
+                  </motion.div>
+                );
+              })}
+            </div>{" "}
+            {/* //? four */}
+            <p
+              className={cn(
+                "text-center mb-10 text-secondary/50 text-2xl italic",
+                playfair.className
+              )}
+            >
+              {content[3].label}
+            </p>
+            <div ref={sidesRef} className="flex px-28 flex-col gap-7">
+              {content[3].info.map((k) => {
+                return (
+                  <motion.div
+                    whileHover="hover"
+                    className="flex cursor-pointer gap-8 justify-between"
+                    key={k.label}
+                  >
+                    <motion.div
+                      className="w-52 h-36 z-10 shrink-0"
+                      transition={{ ease: "easeInOut", duration: 0.4 }}
+                      variants={{ hover: { scale: 1.2 } }}
+                    >
+                      <Image
+                        src={k.image}
+                        alt="image"
+                        width={300}
+                        height={300}
+                        className="w-52 shrink-0 h-36 object-cover rounded-md"
+                      />
+                    </motion.div>
+                    <div className="grow">
+                      <div
+                        className={cn(
+                          "flex text-subOrange text-2xl items-center",
+                          playfair.className
+                        )}
+                      >
+                        <p className="shrink-0">{k.label}</p>
+                        <span className="border-t border-subOrange border-dashed w-full" />
+                        <p>${k.price}</p>
+                      </div>
+                      <p className="text-secondary/50 italic text-base">
+                        {k.description}
+                      </p>
+                    </div>
+                  </motion.div>
+                );
+              })}
+            </div>{" "}
+            {/* //? five */}
+            <p
+              className={cn(
+                "text-center mb-10 text-secondary/50 text-2xl italic",
+                playfair.className
+              )}
+            >
+              {content[4].label}
+            </p>
+            <div ref={dessertsRef} className="flex px-28 flex-col gap-7">
+              {content[4].info.map((k) => {
+                return (
+                  <motion.div
+                    whileHover="hover"
+                    className="flex cursor-pointer gap-8 justify-between"
+                    key={k.label}
+                  >
+                    <motion.div
+                      className="w-52 h-36 z-10 shrink-0"
+                      transition={{ ease: "easeInOut", duration: 0.4 }}
+                      variants={{ hover: { scale: 1.2 } }}
+                    >
+                      <Image
+                        src={k.image}
+                        alt="image"
+                        width={300}
+                        height={300}
+                        className="w-52 shrink-0 h-36 object-cover rounded-md"
+                      />
+                    </motion.div>
+                    <div className="grow">
+                      <div
+                        className={cn(
+                          "flex text-subOrange text-2xl items-center",
+                          playfair.className
+                        )}
+                      >
+                        <p className="shrink-0">{k.label}</p>
+                        <span className="border-t border-subOrange border-dashed w-full" />
+                        <p>${k.price}</p>
+                      </div>
+                      <p className="text-secondary/50 italic text-base">
+                        {k.description}
+                      </p>
+                    </div>
+                  </motion.div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
