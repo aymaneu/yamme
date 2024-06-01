09b7fbaf904360e362bb8ceff29570c5d02a2d88
aymaneu <elgadaymane@gmail.com>
2024-06-01 14:23:10 +0100

    part one finished

---
+++ src/components/aboutUs.tsx
@@ -0,0 +1,55 @@
+import { cn } from "@/utils/cn";
+import { Playfair } from "next/font/google";
+import React from "react";
+import firstChef from "@/../public/firstChef.jpeg";
+import secondChef from "@/../public/secondChef.jpeg";
+import thirdChef from "@/../public/thirdChef.jpeg";
+import forthChef from "@/../public/forthChef.jpeg";
+import Image from "next/image";
+const playfair = Playfair({
+  subsets: ["cyrillic"],
+  style: "italic",
+});
+const AboutUs = () => {
+  return (
+    <div className="text-center mt-20">
+      <p className={cn(playfair.className, "text-subOrange text-[2.5rem]")}>
+        About Us
+      </p>
+      <p className="text-base text-gray-500">
+        Grace was born 20 years ago from a shared vision of culinary excellence
+        and
+        <br /> hospitality, nurtured by a passion for creating unforgettable
+        dining experiences. Our
+        <br /> journey began with a simple desire: to offer a haven where guests
+        could escape the
+        <br /> ordinary and immerse themselves in a world of flavor, elegance,
+        and grace.
+      </p>
+      <div className="border-t border-b border-dashed border-subOrange max-w-xl my-10 py-16 mx-auto">
+        <p className={cn(playfair.className, "text-subOrange text-2xl")}>
+          our dedicated team is the heart and soul behind every exceptional
+          dining experience
+        </p>
+      </div>
+      <div className="grid px-8 gap-8 grid-cols-2">
+        {images.map((i, k) => {
+          return (
+            <Image
+              className="w-[29.5rem] shadow-subOrange/30 shadow-lg h-[31.25rem] object-cover rounded-md"
+              src={i}
+              alt="images"
+              width={300}
+              height={300}
+              key={k}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
+
+const images = [forthChef, thirdChef, secondChef, firstChef];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Header from "../components/header";
 import Ranking from "@/components/ranking";
 import Menu from "@/components/menu";
 import Table from "@/components/table";
+import AboutUs from "@/components/aboutUs";
 export default function Home() {
   return (
     <main className="max-w-5xl h-full min-h-screen border-r border-l border-dashed border-subOrange mx-auto">
@@ -11,6 +12,7 @@ export default function Home() {
       <Ranking />
       <Menu />
       <Table />
+      <AboutUs />
     </main>
   );
 }
